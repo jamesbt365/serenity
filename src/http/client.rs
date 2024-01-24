18bb2b41b106ca22ab4181efef5392f6e3f64939
Gnome! <david2005thomas@Gmail.com>
2024-01-24 17:42:04 +0000

    Remove various instances of `impl AsRef<T>` (#2736)
    This should improve compile times even more, by compiling a lot more
    methods in Serenity, instead of the user's code.

--- src/http/client.rs
+++ src/http/client.rs
@@ -4981,9 +4981,3 @@ fn configure_client_backend(builder: ClientBuilder) -> ClientBuilder {
 fn configure_client_backend(builder: ClientBuilder) -> ClientBuilder {
     builder.use_native_tls()
 }
-
-impl AsRef<Http> for Http {
-    fn as_ref(&self) -> &Http {
-        self
-    }
-}
